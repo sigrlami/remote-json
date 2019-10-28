3db7446c1451cd97aa22b424deb9ce9c25259a36
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2019-10-28 11:45:04 +0200

    add workaround for valid json but wrong content headers

--- remote-json-client/Control/Remote/Monad/JSON/Client.hs
+++ remote-json-client/Control/Remote/Monad/JSON/Client.hs
@@ -6,14 +6,18 @@
 
 module Control.Remote.Monad.JSON.Client
         ( clientSendAPI
+        , clientSendAPIWithAlt
         ) where
 
-import Control.Lens ((^.))
+import Control.Lens ((^.), (.~), (&))
 import Control.Monad (void)
 import Control.Natural
 import Control.Remote.Monad.JSON (SendAPI(..))
 import Data.Aeson
+import Data.ByteString
 import Network.Wreq
+
+--------------------------------------------------------------------------------
 
 -- Public APIs
 -- http://www.raboof.com/projects/jayrock/demo.ashx
@@ -27,3 +31,22 @@ clientSendAPI url = wrapNT $ \ case
           return $ r ^. responseBody
   Async v -> do
           void $ post url (toJSON v)
+
+
+clientSendAPIWithAlt :: String -> (SendAPI :~> IO)
+clientSendAPIWithAlt url = wrapNT $ \ case
+  Sync v -> do
+          r  <- post url (toJSON v)
+          rp <- asJSON $ alterContentTypeToJson r
+          return $ rp ^. responseBody
+  Async v -> do
+          void $ post url (toJSON v)
+
+
+-- | Some APIs return JSON valid content with wrong headers
+--   Use this workaround and try to convert to proper headers
+alterContentTypeToJson :: Response body -> Response body
+alterContentTypeToJson r =
+  if "text/" `isPrefixOf` (r ^. responseHeader "Content-Type")
+    then r & responseHeader "Content-Type" .~ "application/json"
+    else r
